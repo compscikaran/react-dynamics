1f3be3456f82e1f01aa583d91044c8c96cbbc5bd
Karan Gupta <gkaran184@gmail.com>
2023-04-14 17:28:23 +0530

    Redux middleware integrated into Telemetry Service (#21)
    * Signed-off-by: Karan Gupta <gkaran184@gmail.com>
    
    * Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,9 @@
-import withDynamics from "./EmeraldWrapper";
+import withDynamics from "./DynamicsWrapper";
 import { configureAnalytics } from "./service/registrationService";
 import { DynamicEvents } from "./constants";
+import dynamicsMiddleware from "./DynamicsMiddleware";
 export { 
     withDynamics, 
     DynamicEvents, 
-    configureAnalytics };
+    configureAnalytics,
+    dynamicsMiddleware };
